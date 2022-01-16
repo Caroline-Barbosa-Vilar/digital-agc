4d882f0ccaa089a1c9cc6697e748c69ad17b9dcc
Caroline Barbosa <bo.varig@gmail.com>
2022-01-16 17:25:22 -0300

    add js file

---
+++ src/js/panel.js
@@ -0,0 +1,52 @@
+// const panelImages = document.querySelectorAll('.panel-image');
+// const nextArrow = document.getElementById('btn-next');
+// const previousArrow = document.getElementById('btn-previous');
+// let currentImage = 0;
+
+// nextArrow.addEventListener('click', function() {
+//   currentImage++;
+//   panelImages.forEach(image => {
+//     image.classList.remove('show')
+//   });
+//   panelImages[currentImage].classList.add('show');
+// })
+
+const panelImages = document.querySelectorAll('.panel-image');
+const nextArrow = document.getElementById('btn-next');
+const previousArrow = document.getElementById('btn-previous');
+let currentImage = 0;
+
+function hiddeImages() {
+      panelImages.forEach(image => {
+        image.classList.remove('show')
+    });
+}
+
+function showImage() {
+  panelImages[currentImage].classList.add('show');
+}
+
+nextArrow.addEventListener('click', function() {
+
+    const totalImages = panelImages.length - 1;
+    if(currentImage === totalImages) {
+        return;
+    }
+
+    currentImage++;
+    
+    hiddeImages();
+    showImage();
+});
+
+previousArrow.addEventListener('click', function() {
+
+    if(currentImage === 0) {
+        return;
+    }
+
+    currentImage--;
+
+    hiddeImages();
+    showImage();
+})
