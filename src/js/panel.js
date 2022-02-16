63197b3ab87f84d74b6eb3de6ef91daf8849b7de
Caroline Barbosa <bo.varig@gmail.com>
2022-02-16 20:42:19 -0300

    refactoring the panel css

--- src/js/panel.js
+++ src/js/panel.js
@@ -4,37 +4,52 @@ const nextArrow = document.getElementById('btn-next');
 const previousArrow = document.getElementById('btn-previous');
 let currentImage = 0;
 
-function hiddeImages() {
-      panelImages.forEach(image => {
-        image.classList.remove('show')
-    });
-}
-
-function showImage() {
-  panelImages[currentImage].classList.add('show');
-}
-
 nextArrow.addEventListener('click', function() {
 
-    const totalImages = panelImages.length - 1;
-    if(currentImage === totalImages) {
-        return;
-    }
-
     currentImage++;
-    
-    hiddeImages();
-    showImage();
-});
 
-previousArrow.addEventListener('click', function() {
 
-    if(currentImage === 0) {
-        return;
-    }
+    panelImages.forEach(image => {
+        image.classList.remove('show')
+    });
 
-    currentImage--;
+    panelImages[currentImage].classList.add('show');
 
-    hiddeImages();
-    showImage();
 })
+
+
+
+// function hiddeImages() {
+//       panelImages.forEach(image => {
+//         image.classList.remove('show')
+//     });
+// }
+
+// function showImage() {
+//   panelImages[currentImage].classList.add('show');
+// }
+
+// nextArrow.addEventListener('click', function() {
+
+//     const totalImages = panelImages.length - 1;
+//     if(currentImage === totalImages) {
+//         return;
+//     }
+
+//     currentImage++;
+    
+//     hiddeImages();
+//     showImage();
+// });
+
+// previousArrow.addEventListener('click', function() {
+
+//     if(currentImage === 0) {
+//         return;
+//     }
+
+//     currentImage--;
+
+//     hiddeImages();
+//     showImage();
+// })
