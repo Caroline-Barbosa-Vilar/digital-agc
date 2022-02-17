9b55d526caa021ca6b1fe3fcfd84e1d37b783d77
Caroline Barbosa <bo.varig@gmail.com>
2022-02-17 14:26:05 -0300

    panel js commit

--- src/js/panel.js
+++ src/js/panel.js
@@ -1,55 +1,39 @@
-
 const panelImages = document.querySelectorAll('.panel-image');
 const nextArrow = document.getElementById('btn-next');
 const previousArrow = document.getElementById('btn-previous');
 let currentImage = 0;
 
-nextArrow.addEventListener('click', function() {
-
-    currentImage++;
-
-
+function hideImages() {
     panelImages.forEach(image => {
         image.classList.remove('show')
     });
+}
 
-    panelImages[currentImage].classList.add('show');
+function showImage() {
+	panelImages[currentImage].classList.add('show');
+}
 
+nextArrow.addEventListener('click', function() {
+
+	const imagesTotal = panelImages.length - 1;
+    if(currentImage === imagesTotal) {
+        return;
+    }
+	
+	currentImage++;
+	
+	hideImages();
+    showImage();
+});
+
+previousArrow.addEventListener('click', function() {
+
+    if(currentImage === 0) {
+        return;
+    }
+	
+	currentImage--;
+
+    hideImages();
+    showImage();
 })
-
-
-
-// function hiddeImages() {
-//       panelImages.forEach(image => {
-//         image.classList.remove('show')
-//     });
-// }
-
-// function showImage() {
-//   panelImages[currentImage].classList.add('show');
-// }
-
-// nextArrow.addEventListener('click', function() {
-
-//     const totalImages = panelImages.length - 1;
-//     if(currentImage === totalImages) {
-//         return;
-//     }
-
-//     currentImage++;
-    
-//     hiddeImages();
-//     showImage();
-// });
-
-// previousArrow.addEventListener('click', function() {
-
-//     if(currentImage === 0) {
-//         return;
-//     }
-
-//     currentImage--;
-
-//     hiddeImages();
-//     showImage();
-// })
