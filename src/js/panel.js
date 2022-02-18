f0e57364ae8dfa3dc96ec06cf07a12028300402d
Caroline Barbosa <bo.varig@gmail.com>
2022-02-17 21:18:16 -0300

    arrow animation commit

--- src/js/panel.js
+++ src/js/panel.js
@@ -15,7 +15,13 @@ function showImage() {
 
 nextArrow.addEventListener('click', function() {
 
+    previousArrow.classList.remove('hide')
 	const imagesTotal = panelImages.length - 1;
+
+    if (currentImage ===1) {
+        nextArrow.classList.add('hide')
+    }
+
     if(currentImage === imagesTotal) {
         return;
     }
@@ -26,6 +32,12 @@ nextArrow.addEventListener('click', function() {
 });
 
 previousArrow.addEventListener('click', function() {
+
+    nextArrow.classList.remove('hide')
+
+    if (currentImage === 1) {
+        previousArrow.classList.add('hide')
+    }
 
     if(currentImage === 0) {
         return;
